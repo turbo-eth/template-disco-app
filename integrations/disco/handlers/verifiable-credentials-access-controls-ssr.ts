8eeb1d3a029725016b7bdc3c9404ccb107925fff
hexcowboy <accounts@cowboy.dev>
2023-04-18 19:28:23 -0700

    add ssr disco credential protection

---
+++ integrations/disco/handlers/verifiable-credentials-access-controls-ssr.ts
@@ -0,0 +1,23 @@
+import { withIronSessionSsr } from 'iron-session/next'
+import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next'
+
+import { discoClient } from '@/integrations/disco/disco-client'
+import { SERVER_SESSION_SETTINGS } from '@/lib/session'
+
+export function withVerifiableCredentialsAccessControlsSsr<P extends { [key: string]: unknown } = { [key: string]: unknown }>(
+  handler: (context: GetServerSidePropsContext) => GetServerSidePropsResult<P> | Promise<GetServerSidePropsResult<P>>
+) {
+  return withIronSessionSsr(async (context: GetServerSidePropsContext) => {
+    const session = context.req.session
+
+    if (session?.siwe?.address) {
+      const response = await discoClient.get(`/profile/address/${session.siwe.address}`)
+
+      if (response.status === 200 && response.data?.creds) {
+        Object.defineProperty(context.req, 'credentials', { enumerable: true, value: response.data?.creds })
+      }
+    }
+
+    return handler(context)
+  }, SERVER_SESSION_SETTINGS)
+}
